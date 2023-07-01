a104f015188b5936b9a790e6e7abddb8936002d1
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-01 22:40:04 +0200

    Add project files.

---
+++ my_first_cpp_NN_experiment/my_first_cpp_NN_experiment.cpp
@@ -0,0 +1,12 @@
+﻿// my_first_cpp_NN_experiment.cpp : Defines the entry point for the application.
+//
+
+#include "my_first_cpp_NN_experiment.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
