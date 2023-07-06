c2b272f3a06cb619bc3fcaf486dd67edf9304b1c
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-06 20:41:44 +0200

    Added public class content with comments

--- source/model.h
+++ source/model.h
@@ -15,3 +15,61 @@ enum class Activation {
 more readable and type-safe way than using integer constants. In this case, 
 the Activation enumeration is used to represent the type of
 activation function used in the neural network.*/
+
+class Model;
+
+// Base class of computational nodes
+class Node {
+public:
+	Node(Model& model, std::string name);
+	virtual ~Node() {};
+	/*The virtual keyword indicates that the destructor is virtual, which means
+	that it can be overridden by derived classes. This allows derived classes to
+	provide their own implementation of the destructor to perform any additional
+	cleanup specific to the derived class.*/
+
+	//Initialize the node parameters with random values
+	virtual void init(rne_t& rne) = 0;
+
+	//Forward pass.
+	virtual void forward(num_t* inputs) = 0;
+	/*
+	Good enough for initial testing, but in practice this should be replaced
+	with an actual type with a shape defined by data
+	to permit additional validation.
+	*/
+
+	//Backward pass.
+	virtual void reverse(num_t* gradients) = 0;
+	/*Expected inputs during the reverse accumulation phase are the loss
+	gradients with respect to each output.*/
+
+	//Return the number of (learnable) parameters in the node.
+	virtual size_t param_count() const noexcept {
+		return 0;
+	}
+	/*Nodes with no learnable parameters are input & loss nodes.*/
+
+	//Indexing operator for learnable parameters that are altered during training.
+	virtual num_t* param(size_t index) {
+		return nullptr;
+	}
+	/*Nodes without learnable parameters should keep this unimplemented.*/
+
+	//Indexing operator for the losss gradients with respect to learnable parameters.
+	virtual num_t* gradient(size_t index) {
+		return nullptr;
+	}
+	/*Used by an optimizer to adjust the corresponding parameter and potentially
+	for tracking gradient histories (for more sophisitcated optimizers) e.g. AdaGrad*/
+
+	[[nodiscard]] std::string const& name() const noexcept {
+		return name_;
+	}
+	/*The [[nodiscard]] attribute indicates that the caller of the function
+	should not ignore the return value. If the return value is ignored,
+	the compiler may issue a warning.*/
+
+	//For display of node contents.
+	virtual void print() const = 0;
+};
