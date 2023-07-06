a297812861dd8f88892bbbc4a1b664103f321e1e
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-06 19:29:34 +0200

    decided to not use a header for main
    Asa tutorial project I want to lower abstraction by keeping regular #includes directly in file

--- source/my_first_cpp_NN_experiment.h
+++
@@ -1,9 +0,0 @@
-﻿// my_first_cpp_NN_experiment.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-#include <vector>
-
-// TODO: Reference additional headers your program requires here.
