b58317488203aad755e6f29a6d57e80787cafaa1
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-10 11:29:58 +0200

    Added skeleton for FFNode class

---
+++ source/FFNode.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "model.h"
+
+#include <cstdint>
+#include <vector>
+
+//FFNode = Feed Forward Node
+// Constructed as a Fully-connected, feed-forward node/layer
+
+
+class FFNode : public Node {
+public:
+
+private:
+
+};
