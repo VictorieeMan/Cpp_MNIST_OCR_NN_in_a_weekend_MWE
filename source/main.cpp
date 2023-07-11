60df24dc77f50102031850a570ca06b2ea264d2c
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-11 14:28:38 +0200

    declared void train(char* argv[]) funciton

--- source/main.cpp
+++ source/main.cpp
@@ -48,6 +48,69 @@ Model create_model(
 	return model;
 }
 
+void train(char* argv[]) {
+	// Uncomment to debug floating point instability in the network
+	// feenableexcept(FE_INVALID | FE_OVERFLOW);
+
+	std::printf("Executing training routine\n");
+	
+	//>>> TO DO: Test opening files, and fix the path to the data files. Probably needed.
+	std::ifstream images{
+		std::filesystem::path{argv[0]} / "train-images-idx3-ubyte",
+		std::ios::binary
+	};
+
+	std::ifstream labels{
+		std::filesystem::path{argv[0]} / "train-labels-idx1-ubyte",
+		std::ios::binary
+	};
+
+	MNIST* mnist;
+	CCELossNode* loss;
+	Model model = create_model(images, labels, &mnist, &loss);
+
+	model.init();
+
+	// The gradient descent optimizer is stateless, but other optimizers may not
+	// be. Some optimizers need to track "momentum" or gradient histories.
+	// Others may slow the learning rate for each parameter at different rates
+	// depending on various factors.
+	//
+	// F.T.R. Implement an alternative SGDOptimizer that decays the learning
+	// rate over time and compare the results against this optimizer that learns
+	// at a fixed rate.
+	GDOptimizer optimizer{ num_t{0.3} };
+
+	// F.T.R. Here, we've hardcoded the number of batches to train on. In
+	// practice, training should halt when the average loss begins to
+	// vascillate, indicating that the model is starting to overfit the data.
+	// Implement some form of loss-improvement measure to determine when this
+	// inflection point occurs and stop accordingly.
+	size_t i = 0;
+	for (; i != 256; ++i) {
+		loss->reset_score();
+
+		for (size_t j = 0; j != batch_size; ++j) {
+			mnist->forward();
+			loss->reverse();
+		}
+
+		model.train(optimizer);
+	}
+
+	std::printf("Ran %zu batches (%zu samples each)\n", i, batch_size);
+
+	//Print the average loss computed in the final batch
+	loss->print();
+
+	std::ofstream out{
+		std::filesystem::current_path() / (model.name() + ".params"),
+		std::ios::binary
+	};
+
+	model.save(out);
+}
+
 int main(int argc, char* argv[]) {
 	std::cout << "Hello user! Pick a mode of operation." << std::endl;
 	
